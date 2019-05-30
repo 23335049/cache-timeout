443a7d34fc62f83faa01779bffc6b4f46dfbb7ee
王雄飞 <23335049@qq.com>
2019-05-30 15:34:29 +0800

    添加BadJavaFile

---
+++ src/main/java/com/wangn/cachetimeout/demo/BadJavaFile.java
@@ -0,0 +1,18 @@
+package com.wangn.cachetimeout.demo;
+
+/**
+ * class functional description
+ *
+ * @author wang.xiongfei
+ * @version 1.0.0
+ * @since 2019-05-30
+ */
+public class BadJavaFile {
+
+    private String name;
+
+    public String say() {
+        String say = "haha";
+        return null;
+    }
+}
